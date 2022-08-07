ef910dbba14a6b4f0e7d53b87d6175cdad0f419b
Sabiha Bary <sabihaab9@gmail.com>
2022-08-07 13:13:12 -0400

    commit tree 039361aefc22661779eb3ae10bee7d57dc55dfc9

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,16 @@
 class PlantsController < ApplicationController
 
+  # UPDATE /plants
+  def update 
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+
+  rescue ActiveRecord
+    ::RecordNotFound
+    render_not_found_response
+  end
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +29,30 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #DELETE /plants
+
+  def destroy
+    #plant = plant.find_by(id: params[:id])
+    plant = find_plant
+    plant.destroy
+    head :no_content
+    rescue ActiveRecord::RecordNotFound
+      #render json: { error: "plant not found" }, status: :not_found
+      render_not_found_response
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: {error: "Plant not found"}, status: :not_found
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
 end
